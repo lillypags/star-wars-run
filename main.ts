9372a71b4dfc3331d954a1c077f483947ac9fa47
lillypags <63726329+lillypags@users.noreply.github.com>
2020-04-17 10:12:36 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let mySprite = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 1 1 1 1 1 1 1 1 1 1 . . . 
+. . 1 1 1 1 1 1 1 1 1 1 1 1 . . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. f f f f f f f f f f f f f f . 
+. 1 f f f f f f f f f f f f 1 . 
+. 1 f f f f f 1 1 f f f f f 1 . 
+. 1 1 f f f 1 1 1 1 f f f 1 1 . 
+. 1 1 f f 1 1 1 1 1 1 f f 1 1 . 
+. 1 f 1 1 1 1 1 1 1 1 1 1 f 1 . 
+. 1 1 f 1 1 1 f f 1 1 1 f 1 1 . 
+. 1 1 1 f 1 f 1 1 f 1 f 1 1 1 . 
+. . 1 1 1 f f 1 1 f f 1 1 1 . . 
+`, SpriteKind.Player)
